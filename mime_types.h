a3be953a186a5f5fc1652c3f51f74d79a0c8d258
zhoubaozhou <zhoubaozhou@gmail.com>
2013-07-11 16:48:08 +0800

    add mime_types

---
+++ mime_types.h
@@ -0,0 +1,14 @@
+#ifndef HTTP_MIME_TYPES_H
+#define HTTP_MIME_TYPES_H
+
+namespace http {
+namespace server {
+namespace mime_types {
+
+std::string extension_to_type(const std::string& extension);
+
+}
+};
+};
+
+#endif
